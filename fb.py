f541e880dac11d49fcb7950f15d2e3ca93a79636
BladeKnife <66865892+BladeKnife@users.noreply.github.com>
2020-08-21 20:36:22 +0000

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,3 +1,5 @@
 print ("Script Sudah di update ke versi 2 ")
 print ("Update Tambahan menu crack dari pencarian nama")
 print ("silahkan clone ulang / ketik: git pull")
+print ("dan nama file sudah diganti jadi mbf.py")
+print ("jika sudah clone/git pull ketik aja python mbf.py")
